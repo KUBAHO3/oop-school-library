a9db37960819802e43b1e18a376307a805df954d
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-06-19 19:04:23 +0530

    Fix linter errors

--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,14 @@
 require_relative './person'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(specialization, age, name='unknown', parent_permission=true)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
+  def initialize(specialization, age, name = 'unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
