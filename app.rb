2186d4a44f74b484164ecd125147b6495b842310
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-06-22 17:39:08 +0530

    Fix linter errors

--- app.rb
+++ app.rb
@@ -1,11 +1,11 @@
-require_relative './book.rb'
-require_relative './teacher.rb'
-require_relative '.student.rb'
-require_relative '.rental.rb'
+require_relative './book'
+require_relative './teacher'
+require_relative '.student'
+require_relative '.rental'
 
 class App
-  attr_accessor :books, :teachers, :students, :rentals,
-  
+  attr_accessor :books, :teachers, :students, :rentals
+
   def initialize
     @books = []
     @teachers = []
@@ -14,19 +14,23 @@ class App
   end
 
   def book_list
-    @books.each_with_index { |book, index| print "(#{index + 1}) Title: \"#{book.title}\", Author: \"#{book.author}\"\n"}
+    @books.each_with_index do |book, index|
+      print "(#{index + 1}) Title: \"#{book.title}\", Author: \"#{book.author}\"\n"
+    end
   end
 
   def people_list
     people = [*@students, *@teachers]
-    people.each_with_index { |person, index| print "(#{index + 1}) Name: \"#{person.name}\", Age: \"#{person.age}\"\n"}
+    people.each_with_index { |person, index| print "(#{index + 1}) Name: \"#{person.name}\", Age: \"#{person.age}\"\n" }
   end
 
   def rental_list
     puts "Please enter person's id to see rentals"
     id = gets.chomp
     @rentals.each do |rental|
-      print "Date: #{rental.date}, Title: #{rental.book.title}, Author: #{rental.book.author}\n" if id == rental.person.id
+      if id == rental.person.id
+        print "Date: #{rental.date}, Title: #{rental.book.title}, Author: #{rental.book.author}\n"
+      end
     end
   end
 
@@ -61,11 +65,11 @@ class App
 
     case creator
     when 1
-      print "Enter the Name: "
+      print 'Enter the Name: '
       name = gets.chomp.to_s
-      print "Enter the Age: "
+      print 'Enter the Age: '
       age = gets.chomp.to_i
-      print "Enter the Classroom: "
+      print 'Enter the Classroom: '
       classroom = gets.chomp
       new_student = Student.new(age, classroom)
       new_student.name = name
@@ -73,15 +77,15 @@ class App
       print "#{new_student.name} was added successfully"
 
     when 2
-      print "Enter the Name: "
+      print 'Enter the Name: '
       name = gets.chomp
-      print "Enter the Age: "
+      print 'Enter the Age: '
       age = gets.chomp.to_i
-      print "Enter the Specialization: "
+      print 'Enter the Specialization: '
       speecialization = gets.chomp
       new_teacher = Teacher.new(age, name, speecialization)
       @teachers << new_teacher
-      puts "#{new_teacher.name} was added successfully" 
+      puts "#{new_teacher.name} was added successfully"
     end
   end
 end
