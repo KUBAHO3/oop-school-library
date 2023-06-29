6d993a1b7289a1e059a7c59a17c47b42fb77ec84
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-06-29 12:37:21 +0300

    Add student_spec.rb unit test

---
+++ spec/student_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe Student do
+  before :each do
+    @student = Student.new(17, 'Rose', false)
+  end
+  it 'Should return a Student object' do
+    expect(@student).to be_an_instance_of Student
+  end
+
+  describe '#age' do
+    it 'Should return student age' do
+      expect(@student.age).to eql 17
+    end
+  end
+
+  describe '#name' do
+    it 'Should return student name' do
+      expect(@student.name).to eql 'Rose'
+    end
+  end
+  describe '#parent permission' do
+    it 'Should return parent permission' do
+      expect(@student.parent_permission).to be false
+    end
+  end
+end
